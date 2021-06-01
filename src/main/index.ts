7c7c3aee57d73a060fb643d74bfba55e3d7a830d
Heavenstone <malolegendrelemaire@gmail.com>
2021-06-01 23:27:16 +0200

    Cleaned up index

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,31 +3,28 @@ import { app, BrowserWindow } from "electron";
 import * as isDev from "electron-is-dev";
 
 let win = null;
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: false,
+        },
+        backgroundColor: "#292D3E",
+    });
 
-class createWin {
-    constructor() {
-        win = new BrowserWindow({
-            width: 800,
-            height: 800,
-            webPreferences: {
-                nodeIntegration: true,
-                enableRemoteModule: true,
-                webSecurity: false,
-            },
-            backgroundColor: "#292D3E",
-        });
+    const URL = isDev
+        ? `http://localhost:1234`
+        : `file://${path.resolve(__dirname, "dist/renderer/index.html")}`;
 
-        const URL = isDev
-            ? `http://localhost:1234`
-            : `file://${path.resolve(__dirname, "dist/renderer/index.html")}`;
+    console.log(URL);
 
-        console.log(URL);
-
-        win.loadURL(URL);
-    }
+    win.loadURL(URL);
 }
 
-app.whenReady().then(() => new createWin());
+app.whenReady().then(createWindow);
 
 app.on("window-all-closed", () => {
     if (process.platform !== "darwin") {
@@ -37,6 +34,6 @@ app.on("window-all-closed", () => {
 
 app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-        new createWin();
+        createWindow();
     }
 });
