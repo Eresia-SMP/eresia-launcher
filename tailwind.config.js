d2693b84e573a03b49df1ee82752c9974ce6370f
Heavenstone <malolegendrelemaire@gmail.com>
2021-06-01 21:45:27 +0200

    added color to color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                main: { default: "#292D3E", 0: "#292D3E", 1: "#3F4563" },
+                main: {
+                    default: "#292D3E",
+                    0: "#292D3E",
+                    1: "#3F4563",
+                    3: "#4B5169",
+                },
             },
         },
     },
