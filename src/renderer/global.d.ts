21af7a9e2ff6d43e8f2a68119b9780b51c614f83
Heavenstone <malolegendrelemaire@gmail.com>
2021-06-20 19:45:10 +0200

    added McProfileManagerApi to global

--- src/renderer/global.d.ts
+++ src/renderer/global.d.ts
@@ -2,9 +2,11 @@
 /// <reference types="vite/client" />
 
 import type { McVersionManagerApi } from "../common/mcVersionManager";
+import type { McProfileManagerApi } from "../common/profileManager";
 import type { LauncherEventsApi } from "../common/launcherEvents";
 
 declare global {
     const McVersionManager: McVersionManagerApi;
+    const McProfileManager: McProfileManagerApi;
     const LauncherEvents: LauncherEventsApi;
 }
